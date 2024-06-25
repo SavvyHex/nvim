87732eed275f497b49455b63a6a5b58d281d5eff
SavvyHex <paisaketh@gmail.com>
2024-06-25 18:54:47 +0530

    added dashboard

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,6 +7,22 @@ return {
     end,
   },
 
+  {
+    'MeanderingProgrammer/dashboard.nvim',
+    event = 'VimEnter',
+    dependencies = {
+        { 'MaximilianLloyd/ascii.nvim', dependencies = { 'MunifTanjim/nui.nvim' } },
+    },
+    config = function()
+      require('dashboard').setup({
+        header = require('ascii').art.text.neovim.sharp,
+        date_format = '%d/%m/%Y %H:%M:%S',
+        directories = {
+          '~/.config/nvim/',
+        },
+      })
+    end,
+  }
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
