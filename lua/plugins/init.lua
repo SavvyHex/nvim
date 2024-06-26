c7df4c0c1dc137141ff5f59f61b155a702202bc4
SavvyHex <paisaketh@gmail.com>
2024-06-26 22:22:33 +0530

    added support for images

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,21 +8,8 @@ return {
   },
 
   {
-    'MeanderingProgrammer/dashboard.nvim',
-    event = 'VimEnter',
-    dependencies = {
-        { 'MaximilianLloyd/ascii.nvim', dependencies = { 'MunifTanjim/nui.nvim' } },
-    },
-    config = function()
-      require('dashboard').setup({
-        header = require('ascii').art.text.doom.DooM,
-        date_format = '%d/%m/%Y %H:%M:%S',
-        directories = {
-          '~/.config/nvim/',
-        },
-      })
-    end,
-  }
+    "edluffy/hologram.nvim",
+  },
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
