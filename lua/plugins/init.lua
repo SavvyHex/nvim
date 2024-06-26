a30bb62f36891e405a365590a91b17b0e5dd86c1
SavvyHex <paisaketh@gmail.com>
2024-06-26 22:49:24 +0530

    brought back the dashboard

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,6 +10,24 @@ return {
   {
     "edluffy/hologram.nvim",
   },
+
+  {
+    'MeanderingProgrammer/dashboard.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      { 'MaximilianLloyd/ascii.nvim', dependencies = { 'MunifTanjim/nui.nvim' } },
+    },
+    config = function()
+      require('dashboard').setup({
+        header = require('ascii').art.text.doom.DooM,
+          date_format = '%Y-%m-%d %H:%M:%S',
+          directories = {
+            '~/.config',
+            '~/Documents/notes',
+          },
+      })
+    end,
+  }
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
